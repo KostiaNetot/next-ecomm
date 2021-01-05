a0248c00c785d093e4f3ddb5316b74e9a746216f
KostiaNetot <pugjazz@gmail.com>
2021-01-05 08:15:17 +0200

    initial commit

---
+++ next-ecomm_backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f384bb8970cc5c0cc1561837cc5e9e31'),
+    },
+  },
+});
---
+++ next-ecomm_backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3de3def8-9862-4065-bfea-19ee8ed71a23'
+};
